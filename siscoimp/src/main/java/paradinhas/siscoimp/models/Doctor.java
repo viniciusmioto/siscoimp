c13f8f414438057905025bbbd736b3ad07ee499b
gab <gabriel.pontarolo045@gmail.com>
2022-08-28 13:52:28 -0300

    file structure

--- siscoimp/src/main/java/paradinhas/siscoimp/models/Doctor.java
+++ siscoimp/src/main/java/paradinhas/siscoimp/models/Doctor.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package paradinhas.siscoimp;
+package paradinhas.siscoimp.models;
 
 /**
  *
  * @author gab
  */
-public class User extends Person {
-
-    public User() {
-        super(null, null, null, null);
-    }
+public class Doctor {
     
 }
--- siscoimp/src/main/java/paradinhas/siscoimp/models/User.java
+++ siscoimp/src/main/java/paradinhas/siscoimp/models/User.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package paradinhas.siscoimp;
+package paradinhas.siscoimp.models;
 
 /**
  *
  * @author gab
  */
-public class Doctor {
+public class User extends Person {
+
+    public User() {
+        super(null, null, null, null);
+    }
     
 }
